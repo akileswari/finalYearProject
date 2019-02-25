f9c887b86dcd09c0eb7b51a746d2240a03f823f8
akileswari <akileswarikumar@gmail.com>
2019-02-25 23:25:25 +0530

    image downloaded

--- static/Scripts/script.js
+++ static/Scripts/script.js
@@ -1,10 +1,28 @@
+saveData = (function () {
+
+	var a = document.createElement("a");
+	a.style = "display: none";
+	return function (blob, fileName) {
+		var url = window.URL.createObjectURL(blob);
+		a.href = url;
+		a.download = fileName;
+		a.click();
+		window.URL.revokeObjectURL(url);
+	};
+}());
+
 function exportdocx(){
 
   var sImageData = zingchart.exec('myChart', 'getimagedata', {
         format : 'png'
     });
     alert(sImageData);
-//}
+var block = sImageData.split(";");
+var contentType = block[0].split(":")[1];// In this case "image/gif"
+var realData = block[1].split(",")[1];// In this case "R0lGODlhPQBEAPeoAJosM...."
+var blob = b64toBlob(realData, contentType);
+console.log(blob);
+saveData(blob, "a.png");
 var can = document.getElementById('myChart-main-c');
 var ctx = can.getContext('2d');
 
@@ -31,3 +49,27 @@ document.body.appendChild(img);
 //////        window.open("/download");
 //
 }
+function b64toBlob(b64Data, contentType, sliceSize) {
+        contentType = contentType || '';
+        sliceSize = sliceSize || 512;
+
+        var byteCharacters = atob(b64Data);
+        var byteArrays = [];
+
+        for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+            var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+            var byteNumbers = new Array(slice.length);
+            for (var i = 0; i < slice.length; i++) {
+                byteNumbers[i] = slice.charCodeAt(i);
+            }
+
+            var byteArray = new Uint8Array(byteNumbers);
+
+            byteArrays.push(byteArray);
+        }
+
+      var blob = new Blob(byteArrays, {type: contentType});
+      return blob;
+}
+
