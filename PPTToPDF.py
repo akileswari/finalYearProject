d3dfd42a70ae5a263e89485c28b458e38af90f9b
akilak <akileswarik@visualbi.com>
2019-03-08 15:56:36 +0530

    ppt conversion is added

---
+++ PPTToPDF.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import pythoncom
+import win32com.client as client
+
+
+def Ppt_to_pdf(filename,infilepath):
+    """Convert a PPT .pptx to PDF"""
+
+    infile = infilepath
+    outfile = "D:\\finalYearProject\\static\\"+filename+".pdf"
+    formatType = 32
+    pythoncom.CoInitialize()
+    #powerpoint = comtypes.client.CreateObject("Powerpoint.Application")
+    powerpoint = client.Dispatch("Powerpoint.Application")
+    deck = powerpoint.Presentations.Open(infile)
+    deck.SaveAs(outfile, formatType) 
+    deck.Close()
+    powerpoint.Quit()
+
+
