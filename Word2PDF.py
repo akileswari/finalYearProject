0154a41d148dc06c26e686a4e7ac97c5d53b2fd4
akilak <akileswarik@visualbi.com>
2019-03-08 15:58:45 +0530

    updated the functionality

--- Word2PDF.py
+++ Word2PDF.py
@@ -2,23 +2,17 @@ import os
 import sys
 import pythoncom
 import win32com.client as client
-import comtypes.client
-
-
 
 
 def covx_to_pdf(filename):
     """Convert a Word .docx to PDF"""
 
-    infile = "F:\\PycharmProjects\\finalproject\\test1.docx"
-    outfile = "F:\\PycharmProjects\\finalproject\\static\\"+filename+".pdf"
+    infile = "D:\\finalYearProject\\test1.docx"
+    outfile = "D:\\finalYearProject\\static\\"+filename+".pdf"
     wdFormatPDF = 17
-
     pythoncom.CoInitialize()
     word = client.Dispatch("Word.Application")
     doc = word.Documents.Open(infile)
     doc.SaveAs(outfile, FileFormat=wdFormatPDF)
     doc.Close()
     word.Quit()
-
-
